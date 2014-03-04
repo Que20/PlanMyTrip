e2d8bfbdf2ee4c3bc658c54e17e222f629a44691
Dakstraede <alexandre.fayette@gmail.com>
2014-03-04 19:46:10 +0100

    final registration form

--- pages/registration/index.php
+++ pages/registration/index.php
@@ -5,9 +5,11 @@
 
 <div id="regform">
 	<form action="registration.php" method="post">
-		Pseudo : <input type=text class="loginName" name="pseudo">
-		NOM Prénom : <input type=text class="realname" name="realname">
-		Mail : <input type=mail class="mail" name="mail">
-		Mdp  : <input type=password class="mdp" name="mdp">
+		Pseudo : <input type=text class="loginName" name="pseudo"><br>
+		NOM Prénom : <input type=text class="realname" name="realname"><br>
+		Mail : <input type=mail class="mail" name="mail"><br>
+		Mdp  : <input type=password class="mdp" name="mdp"><br>
+		
+		<input type=submit class="validateRegistration" value="Valider">
 	</form>
 </div>
