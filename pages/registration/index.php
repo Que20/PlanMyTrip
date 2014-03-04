c80aa8b23616a04bd18613dc648e63a8cebf4804
Dakstraede <alexandre.fayette@gmail.com>
2014-03-04 19:47:52 +0100

    encodage UTF8 SANS BOM

--- pages/registration/index.php
+++ pages/registration/index.php
@@ -6,7 +6,7 @@
 <div id="regform">
 	<form action="registration.php" method="post">
 		Pseudo : <input type=text class="loginName" name="pseudo"><br>
-		NOM Prénom : <input type=text class="realname" name="realname"><br>
+		NOM PrÃ©nom : <input type=text class="realname" name="realname"><br>
 		Mail : <input type=mail class="mail" name="mail"><br>
 		Mdp  : <input type=password class="mdp" name="mdp"><br>
 		
