726b6ea04c11128621f91824bb1a1897c13ef3e7
Dakstraede <alexandre.fayette@gmail.com>
2014-03-05 00:27:14 +0100

    Implémentation mailing de confirmation (-> modif php.ini)

--- pages/registration/registration.php
+++ pages/registration/registration.php
@@ -30,4 +30,23 @@
 	$request = $bdd->prepare('INSERT INTO User(FullName, Pseudo, Mail, Password, ValidateKey, IsValidate) VALUES(?,?,?,?,?,?)');
 	$request->execute(array($_POST['realname'], $_POST['pseudo'], $_POST['mail'], $mdp, $validate_key, 0));
 	
+	//Envoi du mail de confirmation
+	
+	$message = "Bonjour, et merci pour votre inscription sur notre site ! \n
+				Veuillez suivre ce lien pour activer votre compte : http://planmytrip.com/pages/registration/confirmation.php?".$validate_key."\n \n
+				L'équipe PlanMyTrip. \n \n
+				P.S. : ceci est un mail automatique, veuillez ne pas y répondre.";
+	
+	if(mail($_POST['mail'], "Confirmation d'inscription", $message)){
+		?>
+		<script>alert('Inscription terminée, un mail de confirmation a été envoyé à l\'adresse indiquée !');</script>
+		<?php
+	}
+	else{
+		?>
+		<script>alert('Echec lors de l\'envoi du mail');</script>
+		<?php
+	}
+				
+	
 ?>
