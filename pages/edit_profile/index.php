b15deb1fe4c0eb44b73bf67520bdd9bde5bf9afe
Dakstraede <alexandre.fayette@gmail.com>
2014-03-05 15:46:55 +0100

    Form edit profile

---
+++ pages/edit_profile/index.php
@@ -0,0 +1,39 @@
+<?php
+	include('../topbar.php');
+?>
+
+<div id="edit_profile">
+
+	<div id="edit_infos">
+		Modifiez vos informations<br><br>
+		<form action="edit_profile.php" method="post">
+			<table border="0">
+				<tr>
+			<td>Nom Complet</td><td><input type=text class="regRealname" name="realname"></td>
+				</tr>
+				<tr>
+			<td >Email</td><td><input type=mail class="regMail" name="mail" ></td>
+				</tr>
+			</table>
+			<br><input type=submit class="validateEdition" value="Valider">
+		</form><br>
+	</div>
+	
+	<div id="edit_password">
+		Changez votre mot de passe<br><br>
+		<form action="edit_password.php" method="post">
+			<table border="0">
+				<tr>
+			<td style="width:200px;">Ancien mot de passe</td><td><input type=password class="editmdp" name="realname"></td>
+				</tr>
+				<tr>
+			<td >Nouveau mot de passe</td><td><input type=password class="editmdp" name="mail" ></td>
+				</tr>
+				<tr>
+			<td >Confirmation</td><td><input type=password class="editmdp" name="mail" ></td>
+				</tr>
+			</table>
+			<br><input type=submit class="validateEdition" value="Valider">
+		</form>
+	</div>
+</div>
