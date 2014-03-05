6f21201f9b75c62857a553526c7dffce6b5cca63
Que20 <maarekkevin@gmail.com>
2014-03-05 10:41:54 +0100

    Page result

--- search/index.php
+++ search/index.php
@@ -2,8 +2,14 @@
 session_start();
 include("../pages/topbar.php");
 $s =  $_GET['search'];
+try{
+	$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+	$bdd = new PDO('mysql:host=localhost;dbname=planmytrip', 'root', '', $pdo_options);
+	$bdd->exec("set names utf8");
+}catch (Exception $e){
+	die('Erreur : ' . $e->getMessage());
+}
 ?>
-
 <div id="results">
 	<div id="reultsTitle">
 		Résultat de la cherche pour : <b><?php echo $s ?></b><br>
@@ -16,11 +22,14 @@ $s =  $_GET['search'];
 
 	<div id="resultsItems"><br>
 		<table>
+			<?php
+			$requete = $bdd->query("SELECT * FROM guide WHERE 'Titre'  LIKE '%". mysql_real_escape_string($s) ."%' ORDER BY Id_Guide DESC;");
+			while($item = $requete->fetch()){?>			
 			<tr class="result">
 			<td>
 				<div class="resultInfo">
-					<div class="resultName"><a href="#">KIkoo lol</a></div>
-					<div class="resultAuthor">Ecris par : </div>
+					<div class="resultName"><a href="../pages/guide/index.php?Id_Guide=<?php echo $item['Id_Guide'] ?>"><?php echo $item['Titre'] ?></a></div>
+					<div class="resultAuthor">Ecris par : <?php echo $item['Id_User'] ?></div>
 					Tags : <a href="#"><span class="resultTag"> Culture </span></a>, <a href="#"><span class="resultTag"> Rock </span></a>
 				</div>
 				<div class="resultVote">
@@ -30,20 +39,8 @@ $s =  $_GET['search'];
 				<br>
 			</td>
 			</tr>
-			<tr class="result">
-			<td>
-				<div class="resultInfo">
-					<div class="resultName"><a href="#">KIkoo lol</a></div>
-					<div class="resultAuthor">Ecris par : </div>
-					Tags : <span class="resultTag"> Culture </span>, <span class="resultTag"> Rock </span>
-				</div>
-				<div class="resultVote">
-					<div class="greenthumb"><img class="thumb" src="../img/up.png"><br>25</div>
-					<div class="redthumb"><img class="thumb" src="../img/down.png"><br>25</div>
-				</div>
-				<br>
-			</td>
-			</tr>
+			<?php } ?>
+
 		</table>
 	</div>
 
