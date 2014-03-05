1af13a8524f1ac5fb277294942b030f843e96062
Dakstraede <alexandre.fayette@gmail.com>
2014-03-05 17:47:42 +0100

    controle d'auth sur edit_profile (index.php)

--- pages/edit_profile/index.php
+++ pages/edit_profile/index.php
@@ -1,39 +1,48 @@
 <?php
-	include('../topbar.php');
-?>
-
-<div id="edit_profile">
-
-	<div id="edit_infos">
-		Modifiez vos informations<br><br>
-		<form action="edit_profile.php" method="post" enctype="multipart/form-data">
-			<table border="0">
-				<tr>
-			<td>Nom Complet</td><td><input type=text class="regRealname" name="realname"></td>
-				</tr>
-				<tr>
-			<td >Email</td><td><input type=mail class="regMail" name="mail" ></td>
-				</tr>
-			</table>
-			<br><input type=submit class="validateEdition" value="Valider">
-		</form><br>
-	</div>
+	if(isset($_SESSION['id']) && isset($_SESSION['pseudo'])){
+		
+		include('../topbar.php');
+	?>
 	
-	<div id="edit_password">
-		Changez votre mot de passe<br><br>
-		<form action="edit_password.php" method="post" enctype="multipart/form-data">
-			<table border="0">
-				<tr>
-			<td style="width:200px;">Ancien mot de passe</td><td><input type=password class="editmdp" name="oldmdp"></td>
-				</tr>
-				<tr>
-			<td >Nouveau mot de passe</td><td><input type=password class="editmdp" name="newmdp" ></td>
-				</tr>
-				<tr>
-			<td >Confirmation</td><td><input type=password class="editmdp" name="confnewmdp" ></td>
-				</tr>
-			</table>
-			<br><input type=submit class="validateEdition" value="Valider">
-		</form>
+	<div id="edit_profile">
+	
+		<div id="edit_infos">
+			Modifiez vos informations<br><br>
+			<form action="edit_profile.php" method="post" enctype="multipart/form-data">
+				<table border="0">
+					<tr>
+				<td>Nom Complet</td><td><input type=text class="regRealname" name="realname"></td>
+					</tr>
+					<tr>
+				<td >Email</td><td><input type=mail class="regMail" name="mail" ></td>
+					</tr>
+				</table>
+				<br><input type=submit class="validateEdition" value="Valider">
+			</form><br>
+		</div>
+		
+		<div id="edit_password">
+			Changez votre mot de passe<br><br>
+			<form action="edit_password.php" method="post" enctype="multipart/form-data">
+				<table border="0">
+					<tr>
+				<td style="width:200px;">Ancien mot de passe</td><td><input type=password class="editmdp" name="oldmdp"></td>
+					</tr>
+					<tr>
+				<td >Nouveau mot de passe</td><td><input type=password class="editmdp" name="newmdp" ></td>
+					</tr>
+					<tr>
+				<td >Confirmation</td><td><input type=password class="editmdp" name="confnewmdp" ></td>
+					</tr>
+				</table>
+				<br><input type=submit class="validateEdition" value="Valider">
+			</form>
+		</div>
 	</div>
-</div>
+	<?php
+	else{
+		?>
+		<h1>Accès non autorisé</h1>
+		<?php
+	}
+?>
