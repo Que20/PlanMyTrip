0ad7c2bb207ecd4f1936dc892c2a21b5914bc143
Dakstraede <alexandre.fayette@gmail.com>
2014-03-06 17:53:42 +0100

    Complete admin logger

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -15,7 +15,10 @@
 		<div id="page_admin" style="width:1000px;height:1000px; margin:auto">
 			<div id="adminform" style="width:500px;height:500px; margin-top:200px;margin-left:250px;margin-right:250px;">
 				<form action="login.php" method="post">
-					<table style="width:500px">
+					<table style="width:400px">
+						<tr style="height:50px">
+							<td><h3>Connexion au panneau d'administration</h3><br></td>
+						</tr>
 						<tr>
 							<td>Identifiant : </td><td><input type="text" name="id_admin"></td>
 						</tr>
@@ -23,7 +26,7 @@
 							<td>Mot de passe :</td><td><input type="password" name="mdp_admin"></td>
 						</tr>
 						<tr>
-							<td id="tdr" style="margin:auto"><input type="submit" value="Connexion"></td>
+							<td><input type="submit" value="Connexion"></td>
 						</tr>
 					</table>
 				</form>
