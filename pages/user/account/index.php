9d85e861e88f96bcf5d824d7c6778dddf184d23a
Dakstraede <alexandre.fayette@gmail.com>
2014-03-07 11:27:35 +0100

    ta mère t'as jamais appris à indenter ?

--- pages/user/account/index.php
+++ pages/user/account/index.php
@@ -1,17 +1,23 @@
 <?php
-session_start();
-include("../../../pages/topbar.php");
-if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-{ ?>
-<div id="blanc" style="padding-top:70px;"></div>
-<div id ="accountManagment">
-
-</div>
-
-<script type="text/javascript" src="script.js"></script>
-<?php } else { ?>
-<div id="blanc" style="padding-top:200px;"></div>
-<div id="coError" style="width:500px;margin:auto;"> vous devez être connécté pour afficher cette page !</div>
-<?php } ?>
-<div id="blanc" style="padding-top:300px;"></div>
-<?php include("../../footer.php"); ?>
+	session_start();
+	include("../../../pages/topbar.php");
+	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo'])){ 
+	?>
+		<div id="blanc" style="padding-top:70px;"></div>
+		<div id ="accountManagment">
+		
+		</div>
+		
+		<script type="text/javascript" src="script.js"></script>
+		<?php 
+	} 
+	else { 
+		?>
+		<div id="blanc" style="padding-top:200px;"></div>
+		<div id="coError" style="width:500px;margin:auto;"> vous devez être connécté pour afficher cette page !</div>
+		<?php 
+	} 
+	?>
+	<div id="blanc" style="padding-top:300px;"></div>
+	<?php include("../../footer.php"); 
+?>
